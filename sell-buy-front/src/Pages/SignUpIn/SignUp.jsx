e4c092f0bac58230c54a317637141a21a1c118f2
Kinkriashvili <zhiend0331@gmail.com>
2025-03-26 22:43:26 +0400

    finish making register styles

--- sell-buy-front/src/Pages/SignUpIn/SignUp.jsx
+++ sell-buy-front/src/Pages/SignUpIn/SignUp.jsx
@@ -1,5 +1,50 @@
 import styles from "./signUpIn.module.css";
-
-export default function SignUp() {
-  return <div>Sign Up</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+import { SignForm } from "../../Components/Ui/Inputs";
+import { FormButton } from "../../Components/Ui/Buttons";
+import { SignFormRightSideSvg } from "../../Components/Ui/SvgAnimations";
+export default function SignIn() {
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.leftSide} ${styles.registerForm}`}>
+        <h2>Sign In</h2>
+        <span>Please login to continue yo your account</span>
+        <form action="">
+          <SignForm label={"UserName:"} type={"text"} placholder={"UserName"} />
+          <SignForm label={"Email:"} type={"text"} placholder={"Mail"} />
+          <SignForm
+            label={"Password:"}
+            type={"password"}
+            placholder={"Password"}
+          />
+          <SignForm
+            label={"Repeat Password:"}
+            type={"password"}
+            placholder={"Repeat Password"}
+          />
+          <FormButton
+            handleClick={handleClick}
+            type="submit"
+            className={`${styles.signButton} wht-btn `}
+            text={"Create Account"}
+          />
+        </form>
+        <span className={styles.linkSpam}>
+          Already have an account? <Link to={"/login"}>Sign In</Link>
+        </span>
+      </div>
+      <div>
+        <SignFormRightSideSvg
+          containerClass={`${styles.rightSide} 
+          mainContainer`}
+          title="Welcome!"
+          svgClass={styles.svgContainer}
+        />
+      </div>
+    </div>
+  );
 }

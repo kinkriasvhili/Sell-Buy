11c011190d23f4a20c66a5f617571b0269a4058b
Kinkriashvili <zhiend0331@gmail.com>
2025-04-10 22:49:59 +0400

    add react query to project

--- sell-buy-front/src/Context/AppProvider.jsx
+++ sell-buy-front/src/Context/AppProvider.jsx
@@ -8,26 +8,31 @@ import { FavoriteContextProvider } from "./FavoriteContext";
 import { ReviewContextProvider } from "./ReviewContext";
 import { DiscountContextProvider } from "./DiscountContext";
 import { ChatContextProvider } from "./ChatContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const AppProviders = ({ children }) => {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthContextProvider>
-      <UserContextProvider>
-        <ProductContextProvider>
-          <CartContextProvider>
-            <OrderContextProvider>
-              <FavoriteContextProvider>
-                <ReviewContextProvider>
-                  <DiscountContextProvider>
-                    <ChatContextProvider>{children}</ChatContextProvider>
-                  </DiscountContextProvider>
-                </ReviewContextProvider>
-              </FavoriteContextProvider>
-            </OrderContextProvider>
-          </CartContextProvider>
-        </ProductContextProvider>
-      </UserContextProvider>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <UserContextProvider>
+          <ProductContextProvider>
+            <CartContextProvider>
+              <OrderContextProvider>
+                <FavoriteContextProvider>
+                  <ReviewContextProvider>
+                    <DiscountContextProvider>
+                      <ChatContextProvider>{children}</ChatContextProvider>
+                    </DiscountContextProvider>
+                  </ReviewContextProvider>
+                </FavoriteContextProvider>
+              </OrderContextProvider>
+            </CartContextProvider>
+          </ProductContextProvider>
+        </UserContextProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 };
 
